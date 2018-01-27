c279d22072d53570b8a0f613f7314f91c657bf9e
tarunama <sakeharami@gmail.com>
2018-01-27 17:27:00 +0900

    Add create table function

--- src/exam/db/main.clj
+++ src/exam/db/main.clj
@@ -1,22 +1,24 @@
 (ns exam.db.main
-    (:require [clojure.java.jdbc :as jdbc]))
+  (:require [clojure.java.jdbc :as jdbc]
+            [exam.db.models :as models]))
 
 ; HACK: extract project.clj by environ
 (def db-spec
-    {:dbtype "mysql"
-     :dbname "clj_exam"
-     :host "localhost"
-     :port 3306
-     :user "clj_exam"
-     :password "clj_exam"})
+  {:dbtype   "mysql"
+   :dbname   "clj_exam"
+   :host     "localhost"
+   :port      3306
+   :user     "clj_exam"
+   :password "clj_exam"})
 
-(def fruit-table-ddl
-    (jdbc/create-table-ddl :fruit [[:cost :int]
-                                   [:grade :real]]))
-
-(defn create-table []
-    (jdbc/db-do-commands db-spec [fruit-table-ddl]))
+(defn create-table
+  [table-ddl]
+  (jdbc/db-do-commands db-spec table-ddl))
 
 (defn drop-table
-    [table-name]
-    (jdbc/db-do-commands db-spec (drop-table-ddl table-name)))
+  [table-name]
+  (jdbc/db-do-commands db-spec (jdbc/drop-table-ddl table-name)))
+
+(defn create-all-table
+  []
+  (map create-table models/all-table-ddl))
